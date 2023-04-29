ccb3f34e7005bae99680f0976e4a9cd088c82933
Sidratul Muntaha Sara <sidratul.s@outlook.com>
2023-04-29 19:37:59 +0400

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,12 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+  // const preloader = document.querySelector('#preloader');
+  // if (preloader) {
+  //   window.addEventListener('load', () => {
+  //     preloader.remove();
+  //   });
+  // }
 
   /**
    * Sticky header on scroll
